e461f94e742178c8b32d16721df9799927ce258c
jonathan836 <jonathan999aaa@gmail.com>
2023-12-23 18:25:24 -0400

    NEWS and chatGPT

--- src/app/components/chat-bot/chat-bot.page.ts
+++ src/app/components/chat-bot/chat-bot.page.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { AlertController, LoadingController, ModalController, NavController, NavParams } from '@ionic/angular';
+import { log } from 'console';
 import { AuthService } from 'src/app/services/auth.service';
 import { alertModal } from 'src/app/shared/alert/alert.component';
 import { Constant } from 'src/app/shared/constant/constant.component';
@@ -37,10 +38,16 @@ export class ChatBotPage implements OnInit {
     if (this.navParams.get('statistic1') || this.navParams.get('statistic2')) {
       this.dataStatistic1 = this.navParams.get('statistic1')[0]
       this.dataStatistic2 = this.navParams.get('statistic2')[0]
-      console.log(this.navParams.get('statistic1'));
-      console.log(this.navParams.get('statistic2'));
+      console.log(this.dataStatistic1);
+      console.log(this.dataStatistic2);
 
-      this.assistant(JSON.stringify(this.dataStatistic1) + '\n\n\n' + JSON.stringify(this.dataStatistic2) + '\n\n\n' + 'en forma de tabla')
+      this.assistant(`A continuación, se presenta un resumen comparativo de las estadísticas de Lionel Messi y Test Prueba:
+
+Lionel Messi, de ${this.dataStatistic1.age} años y nacionalidad ${this.dataStatistic1.nationality}, destaca en la ${this.dataStatistic1.league.name} de ${this.dataStatistic1.league.country}. Durante la temporada ${this.dataStatistic1.league.season}, ha participado en ${this.dataStatistic1.game.appearences} partidos, acumulando ${this.dataStatistic1.game.lineups} titularidades y jugando un total de ${this.dataStatistic1.game.minutes} minutos. Messi, con el dorsal número ${this.dataStatistic1.game.number}, desempeña la posición de ${this.dataStatistic1.game.position} y ha logrado una valoración destacada de ${this.dataStatistic1.game.rating}. Además, ha contribuido con ${this.dataStatistic1.goal.total} goles, ${this.dataStatistic1.goal.assists} asistencias y ${this.dataStatistic1.goal.saves} atajadas como capitán.
+
+Por otro lado, Test Prueba, de ${this.dataStatistic2.age} años y origen ${this.dataStatistic2.nationality}, también compite en la ${this.dataStatistic2.league.name} de ${this.dataStatistic2.league.country} en la temporada ${this.dataStatistic2.league.season}. En su única aparición, ha tenido ${this.dataStatistic2.game.lineups} titularidades y ha jugado ${this.dataStatistic2.game.minutes} minutos como ${this.dataStatistic2.game.position}, con el número ${this.dataStatistic2.game.number}. Su valoración es de ${this.dataStatistic2.game.rating}, habiendo anotado ${this.dataStatistic2.goal.total} goles y realizado ${this.dataStatistic2.goal.assists} asistencias. Test Prueba ha registrado ${this.dataStatistic2.goal.saves} atajadas y ha evitado ${this.dataStatistic2.penalty.saved} penales como parte de su destacada actuación.
+
+Ambos jugadores tienen características únicas y contribuyen de manera significativa a sus respectivos equipos.` + '\n\n\n' + 'Un análisis estadístico')
     }
     this.listQuestions()
   }
@@ -69,19 +76,24 @@ export class ChatBotPage implements OnInit {
     // }
     console.log(this.form.controls['prompt'].value);
     if (form.prompt !== '' || form !== '') {
-      this.messages.push({ text: this.form.controls['prompt'].value, isSender: true });
-
       await loadingSpinner(this.loadingCtrl)
 
       if (form.prompt !== '' && form.prompt !== undefined) {
+        this.messages.push({ text: form.prompt, isSender: true });
+
         this.data = {
           prompt: form.prompt
         }
       } else {
+        this.messages.push({ text: form, isSender: true });
+
         this.data = {
           prompt: form
         }
       }
+
+      console.log(this.data);
+
 
       this.form.reset()
 
